3779c3edac0f045f655fdbd5497f52c8674ffeb1
cfg013 <fsats@posteo.com>
2025-11-04 14:48:55 +0100

    Added Cypress.

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+          return null
+        },
+      })
+    },
+    "watchForFileChanges":true,
+    "viewportWidth": 1200,      // Pareto page is bigger than default width
+    //"experimentalPromptCommand": true
+  },  
+});
+
+module.exports = {
+  e2e: {
+    includeShadowDom:true
+  }
+}
